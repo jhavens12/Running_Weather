73c8b254bf643685e9c3b7509768b5c8f8cd167c
jhavens <jhavens12@gmail.com>
2018-04-03 10:01:50 -0400

    initial upload

---
+++ Main.py
@@ -0,0 +1,39 @@
+import get_data
+import ui
+
+
+w,h = ui.get_screen_size()
+view = ui.View(bg_color = 'lightyellow', frame = (0,0,w,h))
+
+bh = 32
+bw = w/2
+sp = 5
+smg = 5
+tmg = 20
+lblh = 32
+lblw = 40
+
+forecast_dict = get_data.forecast_me()
+
+view_1 = ui.ScrollView(frame=(0, tmg, w/3, h-tmg), background_color='orange')
+view_2 = ui.ScrollView(frame=(0+w/3, tmg, w/3, h-tmg), background_color='purple')
+view_3 = ui.ScrollView(frame=(0+((w/3)*2), tmg, w/3, h-tmg), background_color='green')
+
+for n,day in enumerate(forecast_dict):
+    n = n+1
+    label_name = "label"+str(n)
+    view_name = 'view'+str(n)
+    label_name = ui.Label(name = label_name, bg_color ='white', frame = (0, tmg, lblw, lblh))
+        label_name.border_color = 'black'
+        label_name.tint_color = 'black'
+        label_name.border_width = 1
+        label_name.alignment=1
+        label_name.title = forecast_dict[day]['time']['pretty']
+
+        view_name.add_subview(label_name)
+
+view.add_subview(view_1)
+view.add_subview(view_2)
+view.add_subview(view_3)
+
+view.present(style='sheet', hide_title_bar=True)
---
+++ get_data.py
@@ -0,0 +1,48 @@
+import credentials
+import requests
+from pprint import pprint
+import datetime
+
+wu_key = credentials.wu_key
+my_lat = credentials.my_lat
+my_long = credentials.my_long
+
+UTC_adjust = datetime.timedelta(hours=4)
+
+def format_time(date_input,UTC_adjust,time):
+    temp_date = date_input+" "+time
+    datetime_object = datetime.datetime.strptime(temp_date, "%Y-%m-%d %I:%M:%S %p")
+    return datetime_object - UTC_adjust
+
+def twilight(date_input):
+    sunrise_dict = {}
+    url = "https://api.sunrise-sunset.org/json?lat="+my_lat+"&lng="+my_long+"&date="+date_input
+    sunrise_data = requests.get(url).json()
+    sunrise_dict['astronomical_twilight_begin'] = format_time(date_input,UTC_adjust,sunrise_data['results']['astronomical_twilight_begin'])
+    sunrise_dict['nautical_twilight_begin'] = format_time(date_input,UTC_adjust,sunrise_data['results']['nautical_twilight_begin'])
+    sunrise_dict['civil_twilight_begin'] = format_time(date_input,UTC_adjust,sunrise_data['results']['civil_twilight_begin'])
+    sunrise_dict['sunrise'] = format_time(date_input,UTC_adjust,sunrise_data['results']['sunrise'])
+    return sunrise_dict
+
+def forecast_me():
+    forecast_dict = {}
+    term = 'hourly10day'
+    url = "http://api.wunderground.com/api/"+wu_key+"/"+term+"/q/VT/Essex.json"
+    hforecast = requests.get(url).json()
+
+    for hour in hforecast['hourly_forecast']:
+        if hour['FCTTIME']['hour_padded'] == '05':# or hour['FCTTIME']['hour_padded'] == '17':
+            if hour['FCTTIME']['weekday_name'] == 'Tuesday' or hour['FCTTIME']['weekday_name'] == 'Thursday':
+
+                temp_date = hour['FCTTIME']['year'] +"-"+ hour['FCTTIME']['mon'] +"-"+ hour['FCTTIME']['mday']
+                temp_time = hour['FCTTIME']['hour_padded'] +":"+ hour['FCTTIME']['min']+":"+"00"
+                date_key = datetime.datetime.strptime(temp_date+" "+temp_time, '%Y-%m-%d %I:%M:%S')
+
+                forecast_dict[date_key] = {}
+                forecast_dict[date_key]['twilight'] = twilight(temp_date)
+                forecast_dict[date_key]['time'] = hour['FCTTIME']
+                del forecast_dict[date_key]['weather']['UTCDATE']
+                forecast_dict[date_key]['weather'] = hour
+                del forecast_dict[date_key]['weather']['FCTTIME']
+
+    return forecast_dict
