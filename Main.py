90dd5d678e088fc9c460bf67a6b2979b3ee5c3de
jhavens <jhavens12@gmail.com>
2018-04-03 10:18:12 -0400

    fix n

--- Main.py
+++ Main.py
@@ -21,17 +21,17 @@ forecast_dict = get_data.forecast_me()
 
 
 for n,day in enumerate(forecast_dict):
+    frame_x = 0+((w/3)*n)
     n = n+1
     label_name = "label"+str(n)
     view_name = "view_"+str(n)
-    frame_x = 0+((w/3)*n)
     view_name = ui.ScrollView(frame=(frame_x, tmg, w/3, h-tmg), background_color='orange')
     label_name = ui.Label(name = label_name, bg_color ='white', frame = (0, tmg, lblw, lblh))
     label_name.border_color = 'black'
     label_name.tint_color = 'black'
     label_name.border_width = 1
     label_name.alignment=1
-    label_name.title = forecast_dict[day]['time']['pretty']
+    label_name.text = forecast_dict[day]['time']['pretty']
 
     view_name.add_subview(label_name)
     view.add_subview(view_name)
