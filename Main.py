b0302b77960c04c28010b92dd64d2c16c3713d64
jhavens <jhavens12@gmail.com>
2018-06-01 10:30:56 -0400

    fix colors

--- Main.py
+++ Main.py
@@ -65,7 +65,7 @@ def first_run(forecast_dict,view):
         q = 'AM'+str(d)
         view_dict[q] = build.subviews(n,vis,ui,forecast_dict['AM'][day]) #build dictionary
         header = build.headers(n,vis,ui,forecast_dict['AM'][day],view_dict[q],'AM') #n, vis dict, ui object, day info, view_name
-        view_dict[q].add_subview(header)
+        #view_dict[q].add_subview(header)
         imageview = build.imageview_local(n,vis,ui,forecast_dict['AM'][day],view_dict[q])
         view_dict[q].add_subview(imageview)
         if day.hour < 10: #if the hour is truly AM time
@@ -76,7 +76,10 @@ def first_run(forecast_dict,view):
             title_label_list,value_label_list = build.PM_titles_and_values(forecast_dict['PM'][day])
             build.title_labels(n,vis,ui,view_dict[q],title_label_list,'PM')
             build.value_labels(n,vis,ui,view_dict[q],value_label_list,'PM')
+            header.text_color = 'white' #change to white to match PM
+            header.border_color = 'white' #change to white to match PM
             #view_dict[q].background_color = "#0952c6"
+        view_dict[q].add_subview(header)
 
     for n,day in enumerate(forecast_dict['PM']): #Create PM Views
         d = n+1
